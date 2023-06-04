2d01f34db2266a4bdc60860af36eca167ee143d0
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2023-06-04 14:35:09 -0400

    Atualizando tela inicial

--- src/Lembree2.0/App.tsx
+++ src/Lembree2.0/App.tsx
@@ -1,11 +1,10 @@
-import { View } from 'react-native';
-import UserEditScreen from './src/screens/user/UserEditScreen';
-import WelcomeScreen from './src/screens/welcome/WelcomeScreen';
+import { View } from "react-native";
+import WelcomeScreen from "./src/screens/welcome/WelcomeScreen";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <UserEditScreen />
+      <WelcomeScreen />
     </View>
   );
 }
