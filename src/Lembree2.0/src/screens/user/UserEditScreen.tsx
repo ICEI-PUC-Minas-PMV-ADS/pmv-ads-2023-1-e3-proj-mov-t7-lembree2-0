16868a4b14de4d7d9d1828b51210e24632a8e2c5
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2023-06-23 21:46:51 -0400

    Atualizado a tela de usuarios

--- src/Lembree2.0/src/screens/user/UserEditScreen.tsx
+++ src/Lembree2.0/src/screens/user/UserEditScreen.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import {
   Image,
   ImageBackground,
@@ -6,40 +6,34 @@ import {
   TextInput,
   View,
   ViewStyle,
-} from 'react-native';
+} from "react-native";
 
-import bg from '../assets/bg-app-user.png';
-import logo from '../assets/todo-logo.png';
+import bg from "../assets/bg-app-user.png";
+import logo from "../assets/todo-logo.png";
 
-import userStyles from './user.style';
+import userStyles from "./user.style";
 import {
   faCircleArrowLeft,
   faCircleArrowRight,
-} from '@fortawesome/free-solid-svg-icons';
-import TDButton from '../../components/form-elements/button/TDButton';
-import profile from '../assets/mock';
+} from "@fortawesome/free-solid-svg-icons";
+import TDButton from "../../components/form-elements/button/TDButton";
+import profile from "../assets/mock";
 
-function UserEditScreen() {
+function UserEditScreen({ navigation }: { navigation: any }) {
   const handleOnClick = () => {
-    alert('a');
+    navigation.navigate("Home");
   };
 
   return (
     <ImageBackground source={bg} style={userStyles.container}>
       <StatusBar />
       <View style={userStyles.cabecalho}>
-        <TDButton
-          icon={faCircleArrowLeft}
-          onClick={handleOnClick}
-          iconStyle={userStyles.cabecalho__back_btn}
-        />
         <View style={userStyles.cabecalho__line_left} />
         <Image source={logo} />
         <View style={userStyles.cabecalho__line_right} />
-        <View />
       </View>
       <Text style={userStyles.title}>
-        Sempre mantenha seus dados{'\n'}atualizados.
+        Sempre mantenha seus dados{"\n"}atualizados.
       </Text>
       <View style={userStyles.profile}>
         <Image
@@ -54,9 +48,9 @@ function UserEditScreen() {
         />
       </View>
       <View style={userStyles.form}>
-        <TextInput style={userStyles.form__input} />
-        <TextInput style={userStyles.form__input} />
-        <TextInput style={userStyles.form__input} />
+        <TextInput style={userStyles.form__input} placeholder="nome" />
+        <TextInput style={userStyles.form__input} placeholder="email" />
+        <TextInput style={userStyles.form__input} placeholder="telefone" />
       </View>
       <TDButton
         text="Atualizar"
